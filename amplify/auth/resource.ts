3665272ef17a88fd4265ae15e1f781f8497261a6
oleg.cherevko <cherevko.mail@gmail.com>
2024-10-20 18:59:21 +0300

    connect to Bedrock and add UI

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,10 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to AI recipe generator!",
+      verificationEmailBody: (createCode) => `Use this code to confirm your registration: ${createCode()}`,
+    }
   },
 });
--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,35 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+
+const awsRegion = "eu-central-1";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+  "bedrockDS",
+  `https://bedrock-runtime.${awsRegion}.amazonaws.com`,
+  {
+    authorizationConfig: {
+      signingRegion: awsRegion,
+      signingServiceName: "bedrock",
+    },
+  }
+)
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      `arn:aws:bedrock:${awsRegion}::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0`,
+    ],
+    actions: ["bedrock:InvokeModel"],
+    
+  })
+)
